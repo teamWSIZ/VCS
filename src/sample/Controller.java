e42c39e816f4ba72a9dc824fa3b9030a0cf8e070
pmarecki <pmarecki@gmail.com>
2017-10-07 09:02:36 +0200

    Reorganizacja

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextField input;
+    @FXML
+    TextField output;
+
+
+    public void funkcja() {
+        System.out.println("hello");
+    }
+
 }
