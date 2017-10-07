fe73998801d13d75a4646a3c4caacec10a18b515
pmarecki <pmarecki@gmail.com>
2017-10-07 09:31:45 +0200

    Próba dodania buttonu dynamicznie

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,25 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
     TextField input;
+
     @FXML
     TextField output;
 
+    @FXML
+    VBox dolnybox;
+
+    public void initialize() {
+        System.out.println("Startujemy");
+        Button b = new Button("Drugi button");
+        dolnybox.getChildren().add(b);
+    }
 
     public void funkcja() {
         System.out.println("hello");
