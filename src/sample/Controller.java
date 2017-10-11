8f7e1512b13c02862c55059b919fdcc5710c8066
Marcin <freestyle3001@gmail.com>
2017-10-11 20:29:03 +0200

    Dodano slider,przycisk oraz TextField'a

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -8,6 +9,25 @@ public class Controller {
     TextField input;
     @FXML
     TextField output;
+    @FXML
+    TextField sliderTextField;
+    @FXML
+    Slider slider;
+
+    public void getValueFromSliderToTextField(){
+        double val = slider.getValue();
+        sliderTextField.setText(String.valueOf((int)val));
+    }
+
+    public void setValueFromTextFieldToSlider(){
+        try {
+            String text = sliderTextField.getText();
+            slider.setValue(Double.valueOf(text));
+        }catch (Exception e){
+            System.out.println("Error! To nie jest cyfra!");
+        }
+
+    }
 
 
     public void funkcja() {
