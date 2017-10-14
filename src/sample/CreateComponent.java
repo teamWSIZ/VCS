cf87e27e83afb6fbdc3742ceab6ab562914e2ab7
Piotr488 <piotrkubala92@gmail.com>
2017-10-14 09:13:07 +0200

    Klasa Zbroja

--- src/sample/CreateComponent.java
+++ src/sample/CreateComponent.java
@@ -1,5 +1,26 @@
 package sample;
 
+class Zbroja {
+    int down;
+    int count;
+
+    public int getDown() {
+        return down;
+    }
+
+    public void setDown(int down) {
+        System.out.println("Wartość zmiennej down zmieniana z " + this.down + " na " + down);
+        this.down = down;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
 public class CreateComponent {
     public static void main(String[] args) {
 
