88606d0ee64aa231f6f18702872e6f5f73a58071
Piotr488 <piotrkubala92@gmail.com>
2017-10-14 09:34:28 +0200

    Tworzenie Componentu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
@@ -14,6 +17,8 @@ public class Controller {
     Slider slider;
     @FXML
     Label lbl_progress_bar_value;
+    @FXML
+    HBox zbrojaPane;
 
     public void funkcja() {
         txt_output.setText(txt_input.getText());
@@ -21,5 +26,12 @@ public class Controller {
 
     public void changeValue(){
         lbl_progress_bar_value.setText(String.valueOf(slider.getValue()));
+    }
+
+    public void dodajZbroje(ActionEvent actionEvent) {
+        Button b = new Button("Save");
+        TextField tf_down = new TextField();
+        TextField tf_count = new TextField();
+        zbrojaPane.getChildren().addAll(tf_count, tf_down, b);
     }
 }
