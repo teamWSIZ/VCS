09104fd0f1d5cebf223f978fcfea18a45b5b6838
Piotr488 <piotrkubala92@gmail.com>
2017-10-14 09:15:15 +0200

    Merge z branchem Component

--- src/sample/CreateComponent.java
+++ src/sample/CreateComponent.java
@@ -22,7 +22,20 @@ class Zbroja {
     }
 }
 public class CreateComponent {
+    //dialog
+    public static void edytujLiczbe(Zbroja zbroja) {
+        zbroja.down += 10;
+    }
+
+
     public static void main(String[] args) {
+        Zbroja nasza = new Zbroja();
+        nasza.setDown(12);
+
+        //
+        System.out.println(nasza.getDown());
+        edytujLiczbe(nasza);    //przekazuje instancje klasy (referencje do obiektu)
+        System.out.println(nasza.getDown());
 
     }
 }
