92fcf04519ebf1142ee62e7a03bf4e396f15f778
Marcin <freestyle3001@gmail.com>
2017-10-28 14:10:58 +0200

    News Update

---
+++ src/zajecia3/components/NewNewsDialog.java
@@ -0,0 +1,81 @@
+package zajecia3.components;
+
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import zajecia3.model.News;
+import zajecia3.service.ApplicationService;
+
+import java.util.Optional;
+
+public class NewNewsDialog {
+    Dialog<News> dialog;
+    News news;
+
+    public NewNewsDialog() {
+        arrangeDialogElements();
+    }
+
+    public void setNews(News news) {
+        this.news = news;
+    }
+
+    public News getNews(){
+        return news;
+    }
+    private void arrangeDialogElements() {
+        dialog = new Dialog<>();
+        dialog.setTitle("New News");
+
+        Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
+
+        ButtonType loginButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField titleOfNews = new TextField();
+        TextArea fulltext = new TextArea();
+        titleOfNews.setPromptText("Title of News");
+        fulltext.setPromptText("Insert News!");
+
+        grid.add(titleOfNews, 1, 0);
+        grid.add(new Label("Title:"), 0, 0);
+        grid.add(fulltext, 1, 1);
+        grid.add(new Label("Text:"), 0, 1);
+
+        Node addButton = dialog.getDialogPane().lookupButton(loginButtonType);
+
+        titleOfNews.textProperty().addListener((observable, oldValue, newValue) -> {
+            addButton.setDisable(newValue.isEmpty());
+        });
+
+        fulltext.textProperty().addListener((observable, oldValue, newValue) -> {
+            addButton.setDisable(newValue.isEmpty());
+        });
+
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(() -> titleOfNews.requestFocus());
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new News(titleOfNews.getText(),fulltext.getText());
+            }
+            return null;
+        });
+        dialog.showAndWait().ifPresent(news -> {
+            setNews(news);
+        });
+    }
+}
---
+++ src/zajecia3/model/News.java
@@ -0,0 +1,62 @@
+package zajecia3.model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class News {
+    SimpleIntegerProperty id = new SimpleIntegerProperty(1);
+    SimpleStringProperty title;
+    SimpleStringProperty fulltext;
+
+
+    public News() {
+    }
+
+    public News(String title, String fulltext) {
+        this.title = new SimpleStringProperty(title);
+        this.fulltext = new SimpleStringProperty(fulltext);
+    }
+
+    public News(Integer id,String title ,String fulltext) {
+        this.id = new SimpleIntegerProperty(id);
+        this.title = new SimpleStringProperty(title);
+        this.fulltext = new SimpleStringProperty(fulltext);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public SimpleStringProperty titleProperty() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    public String getFulltext() {
+        return fulltext.get();
+    }
+
+    public SimpleStringProperty fulltextProperty() {
+        return fulltext;
+    }
+
+    public void setFulltext(String fulltext) {
+        this.fulltext.set(fulltext);
+    }
+}
+
